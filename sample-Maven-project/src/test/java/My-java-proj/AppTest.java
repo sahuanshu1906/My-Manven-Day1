d0a2b59930b5aba269b7d7264c0de7b6c6946648
sahuanshu1906 <sahuanshu1913@gmail.com>
2025-01-05 23:56:34 +0530

    src and pom.xml files added

---
+++ sample-Maven-project/src/test/java/My-java-proj/AppTest.java
@@ -0,0 +1,19 @@
+package My-java-proj;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
